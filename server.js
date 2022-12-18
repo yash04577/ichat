f54d777e1a1d7bc5bf19988038ab7af9357222e3
yash <yash04577@gmail.com>
2022-12-18 13:46:27 +0530

    second test

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const http = require("http").createServer(app)
 app.use(express.static(__dirname + "/public"))
 const port = process.env.PORT || 8000
 
-http.listen(port, ()=>{
-    console.log(`listining at port ${port}`)
-})
-
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/index.html")
 })
@@ -17,10 +13,14 @@ const io = require("socket.io")(http);
 
 io.on('connection', (socket)=>{
     console.log("socket connection successfull");
-
+    
     socket.on('message', (msg)=>{
         socket.broadcast.emit('message', msg)
     })
 })
 
+
+http.listen(port, ()=>{
+    console.log(`listining at port ${port}`)
+})
 module.exports = app;
