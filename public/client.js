61106730205ea7a90b2d1f2d25de71126b634011
yash <yash04577@gmail.com>
2022-12-19 16:43:07 +0530

    frontend added

--- public/client.js
+++ public/client.js
@@ -1,19 +1,26 @@
 const socket = io();
+const input_message = document.getElementById("input_message");
 const send_btn = document.getElementById("send_btn");
 
 const message_container = document.getElementsByClassName("message_container")[0];
 let uname;
 
-do{
+// do{
 
-    uname = prompt("enter your name");
-}while(!uname)
+//     uname = prompt("enter your name");
+// }while(!uname)
 
-send_btn.addEventListener('keyup', (e)=>{
+input_message.addEventListener('keyup', (e)=>{
     if(e.key === "Enter"){
-        console.log(e.target.value);
+        // console.log(e.target.value);
         send_msg(e.target.value);
     }
+})
+
+send_btn.addEventListener('click', ()=>{
+    
+    send_msg(input_message.value);
+    
 })
 
 function send_msg(msg){
@@ -30,8 +37,7 @@ function send_msg(msg){
 function appendmsg(message, type){
     let main_div = document.createElement('div');
     main_div.classList.add(type);
-
-
+   
     if(type == "message_incoming"){
         let markup = `
         <div class="sender">${message.user}</div>
@@ -39,6 +45,8 @@ function appendmsg(message, type){
         `
         main_div.innerHTML = markup;
         message_container.appendChild(main_div);
+        console.log(message.user);
+        scrollToBottom();
     }
 
     else{
@@ -47,12 +55,19 @@ function appendmsg(message, type){
         `
         main_div.innerHTML = markup;
         message_container.appendChild(main_div);
+        console.log(message.user);
+        scrollToBottom();
     }
+    scrollToBottom();
 
-   
-   
+}
+
+function scrollToBottom(){
+    message_container.scrollTop = message_container.scrollHeight;
+
 }
 
 socket.on('message', (msg) => {
     appendmsg(msg, "message_incoming")
+    scrollToBottom();
 })
